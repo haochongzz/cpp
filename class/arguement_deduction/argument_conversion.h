f8465a786317f162e7334c13eada1f8cd473799c
Haochong Zhang <104999246+haochongzz@users.noreply.github.com>
2023-06-12 10:02:30 +0800

    Create argument_conversion.h

---
+++ class/arguement_deduction/argument_conversion.h
@@ -0,0 +1,4 @@
+// const conversions and array or function to pointer are the only automatic
+// conversions for arguments to parameters with template types.
+
+// function template explicit argument
